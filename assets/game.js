8b9b8ad53590043f56fde6d1a93b474758243160
ZuuZaa <zumbaghirova@gmail.com>
2019-04-14 18:29:50 +0400

    set choices

--- assets/game.js
+++ assets/game.js
@@ -12,7 +12,13 @@ firebase.initializeApp(config);
 var database = firebase.database()
 var player1 = null
 var player2 = null
-var key = ""
+var key1 = null
+var key2 = null
+var choice1 = null
+var choice2 = null
+var win1 = 0
+var win2 = 0
+
 
 
 function checkUsers() {
@@ -20,63 +26,93 @@ function checkUsers() {
         player1 = $("#uname").val()
         database.ref("/player1/").set({
             name: player1,
-            wins: 0,
-            losses: 0,
-            choice: ""
+            choice: choice1,
+            win: win1
         })
+        window.localStorage.setItem("player", "player1")
+        key1 = window.localStorage.getItem("player")
         setPlayer1Name()
-        window.localStorage.setItem("player","player1")
-        key = window.localStorage.getItem("player")
 
     } else if (player2 == null) {
         player2 = $("#uname").val()
         database.ref("/player2/").set({
-            name: $("#uname").val(),
-            wins: 0,
-            losses: 0,
-            choice: ""
+            name: player2,
+            choice: choice2,
+            win: win2
         })
+        window.localStorage.setItem("player", "player2")
+        key2 = window.localStorage.getItem("player")
         setPlayer2Name()
-        window.localStorage.setItem("player","player2")
-        key = window.localStorage.getItem("player")
     }
 }
 
 function setPlayer1Name() {
-    alert("1")
-    $("#player1").html($("#uname").val())
-    $("#player2").html("waiting for Player2")
-    $(".score").html("welcome,   " + $("#uname").val() + "  !")
-    if ( player2 != null && key == "player1") {
-        $("#player2").html(player2.name)
-        console.log(player2.name)
+    if (!player2) {
+        alert("1")
+        $("#player1").html($("#uname").val())
+        $("#player2").html("waiting for Player2")
+        $(".score").html("welcome,   " + $("#uname").val() + "  !")
+        $("#player1img").attr("src", "assets/images/emoji-clipart-595626-5131388.png")
     }
-
+    
 }
 function setPlayer2Name() {
     alert("2")
-    console.log(player1.name)
     $("#player1").html(player1.name)
+    $("#player1img").attr("src", "assets/images/emoji-clipart-595626-5131388.png")
     $("#player2").html($("#uname").val())
     $(".score").html("welcome,   " + $("#uname").val() + "  !" + "<br>" + "Let's gooooo")
+    $("#player2img").attr("src", "assets/images/1495750744Winking-emoticon-emoji-Clipart-info.png")
 }
+function nameSet() {
+
+    if (player2 != null && key2 == null) {
+        $("#player2").html(player2.name)
+        $("#player2img").attr("src", "assets/images/1495750744Winking-emoticon-emoji-Clipart-info.png")
+        alert("player 2 is ready")
+    }
+
+}
+
 database.ref().on("value", function (snapshot) {
     console.log(snapshot.val())
     if (snapshot.val()) {
         if (snapshot.val().player1) {
             player1 = snapshot.val().player1
+            choice1 = snapshot.val().player1.choice
         }
         if (snapshot.val().player2) {
             player2 = snapshot.val().player2
+            choice2 = snapshot.val().player2.choice
         }
     }
+    nameSet()
 })
 
 $("form").on("submit", function (e) {
     e.preventDefault()
     checkUsers()
-    
+
     $("#signin").addClass("d-none")
+    $("#welcomeText").addClass("d-none")
     $("#gameZone").removeClass("d-none")
+    $("#gameButtons").removeClass("d-none")
+})
+
+$(".my-btn").on("click", function () {
+    alert("my-btn")
+    if (key2 == null) {
+        choice1 = $(this).data("name")
+        database.ref("/player1/").update({
+            choice: choice1
+        })
+    }
+    if (key1 == null) {
+        choice2 = $(this).data("name")
+        database.ref("/player2/").update({
+            choice: choice2
+        })
+    }
+
 })
 
